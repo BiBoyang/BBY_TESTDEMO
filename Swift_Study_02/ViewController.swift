fd25830d151a2f264d28805888e89b908b18f122
Boyang <biboyang0@126.com>
2020-07-06 23:36:07 +0800

    no message

---
+++ Swift_Study_02/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift_Study_02
+//
+//  Created by 毕博洋 on 2020/7/6.
+//  Copyright © 2020 毕博洋. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        self.view.backgroundColor = UIColor.red
+        
+        
+        
+    }
+
+
+}
+
