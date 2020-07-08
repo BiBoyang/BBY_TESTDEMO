822a685533dcb807a250d165b03ef84666942b34
Boyang <biboyang0@126.com>
2020-07-08 18:15:29 +0800

    no message

---
+++ SwiftUI_Study_01/SwiftUI_Study_01/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI_Study_01
+//
+//  Created by 毕博洋 on 2020/7/8.
+//  Copyright © 2020 毕博洋. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
